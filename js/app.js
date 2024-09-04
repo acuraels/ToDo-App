96764347b04c5854b34f2f59d46a4a1041479a30
acuraels <daniil.rib@mail.ru>
2024-09-04 17:05:19 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const inputBox = document.querySelector('.input-box');
+const listContainer = document.querySelector('.list-container');
+
+function addTask() {
+    if (inputBox.value.trim() === '') {
+        alert('Please enter a task!');
+        return;
+    }
+
+    else {
+        let li = document.createElement('li');
+        li.textContent = inputBox.value;
+
+        // Создаем кнопку удаления
+        let deleteButton = document.createElement('div');
+
+        deleteButton.textContent = '×'; // Символ удаления (крестик)
+        deleteButton.className = 'delete-btn';
+        deleteButton.onclick = function () {
+            li.remove();  // Удаляем задачу
+        };
+
+        li.appendChild(deleteButton);  // Добавляем кнопку удаления в <li>
+
+        li.addEventListener('click', function () {
+            li.classList.toggle('checked');
+        });
+
+        listContainer.appendChild(li);
+        inputBox.value = '';
+    }
+}
