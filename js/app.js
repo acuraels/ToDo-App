248e319354cda62caf4ecbd7dce6a22e58660188
acuraels <daniil.rib@mail.ru>
2024-09-05 12:17:57 +0500

    lang-commit

--- js/app.js
+++ js/app.js
@@ -77,3 +77,19 @@ inputBox.addEventListener('keydown', function (event) {
 
 // Загружаем задачи при загрузке страницы
 document.addEventListener('DOMContentLoaded', loadTasks);
+
+
+// Функция для показа/скрытия меню
+function toggleMenu() {
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+
+    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+}
+
+// Закрытие меню при клике вне его области
+window.onclick = function (event) {
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+
+    if (!event.target.closest('.burger-menu') && !event.target.closest('.dropdown-menu'))
+        dropdownMenu.style.display = 'none';
+}
